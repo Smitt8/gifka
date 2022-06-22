8546130e0b58460b73dc9f1faf1e1c9a580b7cdf
Igor Prokazov <smitt8@yandex.ru>
2022-06-22 23:06:47 +0300

    feat: Рализовать класс Api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,15 @@
 import "./index.css";
+import Api from "../components/api.js";
+import {
+  giphyConfig
+} from "../utils/consts.js"
+
+const api = new Api(giphyConfig);
+
+api.loadTrending()
+  .then(trends => {
+    console.log(trends);
+  })
+  .catch(err => {
+    console.log(err);
+  })
---
+++ src/utils/consts.js
@@ -0,0 +1,4 @@
+export const giphyConfig = {
+  url: "api.giphy.com",
+  token: "Q0QCYBXGQAPMAZ0TQGE7d91ZammEhL08"
+}
