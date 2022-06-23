4cf92f7a0452802225e5f15a07dea6d3a937d7fb
natalai2410 <58889273+natalai2410@users.noreply.github.com>
2022-06-23 19:38:02 +0300

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import "./index.css";
+
+
 import Api from "../components/api.js";
 import Form from "../components/Form.js";
+
+import Section from "../components/Section.js";
+import Gif from "../components/Gif.js";
+
 import {
   giphyConfig,
   formConfig
@@ -10,18 +16,26 @@ const api = new Api(giphyConfig);
 
 const searchForm = new Form(formConfig, handleSearchGif);
 
+function addGif(link) {
+  const giphy = new Gif(link, '#template-item');
+  return giphy.generateItem();
+}
+
 /**
- * 
- * @param {Event} event 
- * @param {*} inputs 
+ *
+ * @param {Event} event
+ * @param {*} inputs
  */
+const listContainer = new Section((giphy) => {
+  listContainer.addItem(addGif(giphy.link));
+}, ".animation-images__list");
 
 function handleSearchGif(event, values) {
   event.preventDefault();
-
   api.searchGifs(values["search-query"])
-  .then(gifs => {
-    console.log(gifs);
+  .then( data => {
+    // console.log(data.id);
+    listContainer.addItem(addGif(`https://media1.giphy.com/media/${data.id}/giphy.gif`))
   })
   .catch(err => {
     console.log(err);
