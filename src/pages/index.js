89978d90e6e353260984d8707b85b6e95b1376fe
natalai2410 <58889273+natalai2410@users.noreply.github.com>
2022-06-24 17:44:37 +0300

    #переключение  табов тренды

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import "./index.css";
+// import "./index.css";
 
 
 import Api from "../components/api.js";
@@ -44,4 +44,95 @@ function handleSearchGif(event, values) {
   })
 }
 
+function handleTrendGif() {
+  api.loadTrending()
+    .then( giphy => {
+      listContainer.free();
+      giphy.data.forEach(item => {
+        listContainer.addItem(addGif(`https://media1.giphy.com/media/${item.id}/giphy.gif`));
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+function handleRandomGif() {
+  api.getRandom()
+    .then( (data) => {
+      listContainer.free();
+      //console.log(data.id);
+      // listContainer.addItem(addGif(`https://media1.giphy.com/media/${giphy.id}/giphy.gif`));
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
 searchForm.setEventsListeners();
+
+//подумать!
+
+const btnRandom = document.querySelector('.tab-item_random');
+const btnTrend = document.querySelector('.tab-item-trend');
+const btnNew = document.querySelector('.tab-item-new');
+const btnSearch = document.querySelector('.tab-item-search');
+const searchLink = document.querySelector('.search-link');
+
+
+btnRandom.addEventListener('click', btnRandomClick);
+btnTrend.addEventListener('click', btnTrendClick);
+btnNew.addEventListener('click', btnNewClick);
+btnSearch.addEventListener('click', btnSearchClick);
+
+
+function btnRandomClick() {
+  btnRandom.classList.add("tab-item_selected");
+
+  btnNew.classList.remove("tab-item_selected");
+  btnTrend.classList.remove("tab-item_selected");
+  btnSearch.classList.remove("tab-item_selected");
+
+  searchLink.classList.remove("search-link_opened");
+
+  handleRandomGif();
+}
+
+
+
+function btnTrendClick() {
+  btnTrend.classList.add("tab-item_selected");
+
+  btnRandom.classList.remove("tab-item_selected");
+  btnNew.classList.remove("tab-item_selected");
+  btnSearch.classList.remove("tab-item_selected");
+
+  searchLink.classList.remove("search-link_opened");
+
+  handleTrendGif();
+}
+
+
+
+function btnNewClick() {
+  btnNew.classList.add("tab-item_selected");
+
+  btnRandom.classList.remove("tab-item_selected");
+  btnTrend.classList.remove("tab-item_selected");
+  btnSearch.classList.remove("tab-item_selected");
+
+  searchLink.classList.remove("search-link_opened");
+}
+
+
+
+function btnSearchClick() {
+  btnSearch.classList.add("tab-item_selected");
+
+  btnRandom.classList.remove("tab-item_selected");
+  btnTrend.classList.remove("tab-item_selected");
+  btnNew.classList.remove("tab-item_selected");
+
+  searchLink.classList.add("search-link_opened");
+}
+
